f5cd598a3a90ff51af2cff402be32f9a9163a41d
Jonathan B Coe <jbcoe@users.noreply.github.com>
2017-02-06 15:59:24 +0000

    Windows fixes for build.py

--- scripts/build.py
+++ scripts/build.py
@@ -1,58 +1,117 @@
 #!/usr/bin/env python
+
+import argparse
 import sys
 import os
-import platform
 import subprocess
+import platform
 
+script_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.dirname(script_dir)
+default_build_dir = os.path.join(script_dir, '../build')
 
-def main():
-    import argparse
-    parser = argparse.ArgumentParser()
+def _build_parser():
+    parser = argparse.ArgumentParser(
+            description='Runs CMake with platform-appropriate arguments')
+
     parser.add_argument(
-        '-t', help='run tests', action='store_true', dest='run_tests')
+        '-p',
+        '--prepare',
+        help='prepare a build with cmake',
+        dest='prepare',
+        action='store_true')
+
     parser.add_argument(
-        '-v', help='verbose', action='store_true', dest='verbose')
+        '-t',
+        '--tests',
+        help='run all tests with cmake',
+        dest='tests',
+        action='store_true')
+
     parser.add_argument(
         '-o',
-        help='output dir (relative to source dir)',
-        default='build',
-        dest='out_dir')
+        '--output',
+        help='path for cmake build',
+        dest='output_path',
+        default=default_build_dir)
+
     parser.add_argument(
         '-c',
-        help='config (Debug or Release)',
-        default='Debug',
-        dest='config')
+        '--configuration',
+        help='build configuration',
+        dest='build_config',
+        default='Debug')
+
     parser.add_argument(
         '--python-path',
         help='path to python executable ie "/usr/local/bin/python3"',
         dest='python_path')
-    args = parser.parse_args()
-    args.platform = platform.system()
-    
-    src_dir = os.path.dirname(os.path.dirname(__file__))
 
-    cmake_invocation = ['cmake', '.','-B{}'.format(args.out_dir)]
-    if args.platform == 'Windows':
-        cmake_invocation.extend(['-G', '"Visual Studio 14 2015 Win64"'])
+    if platform.system() == 'Windows':
+        parser.add_argument(
+            '--win32',
+            help='Build 32-bit libraries',
+            action='store_true',
+            dest='win32')
+
+    return parser.parse_args()
+
+
+def _get_cmake_invocation(args):
+    if platform.system() == 'Windows':
+        invocation = ['cmake', root_dir, '-B{}'.format(args.output_path)]
+        if args.win32:
+            invocation.extend(['-G', 'Visual Studio 14 2015'])
+        else:
+            invocation.extend(['-G', 'Visual Studio 14 2015 Win64'])
     else:
-        cmake_invocation.append('-DCMAKE_BUILD_TYPE={}'.format(args.config))
-
-    if args.verbose:
-        cmake_invocation.append('-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON')
+        invocation = ['cmake', root_dir, '-G', 'Ninja']
+        invocation.append('-DCMAKE_BUILD_TYPE={}'.format(args.build_config))
+    
     if args.python_path:
-        cmake_invocation.append('-DPYTHON_EXECUTABLE={}'.format(args.python_path))
-
-    subprocess.check_call(cmake_invocation, cwd=src_dir)
-    subprocess.check_call(
-        'cmake --build ./{}'.format(args.out_dir).split(), cwd=src_dir)
-
-    if args.run_tests:
-        rc = subprocess.call(
-            'ctest . --output-on-failure -C {}'.format(args.config).split(),
-            cwd=os.path.join(src_dir, args.out_dir))
-        if rc != 0:
-            sys.exit(1)
+        invocation.append('-DPYTHON_EXECUTABLE={}'.format(args.python_path))
+    
+    return invocation
 
 
-if __name__ == '__main__':
+def _get_build_invocation(args):
+    invocation = ['cmake', '--build', args.output_path]
+    if platform.system() == 'Windows':
+        invocation.extend(['--config', args.build_config, '--', '-m'])
+    if platform.system() != 'Windows':
+        invocation.append('--')
+        if args.force:
+            invocation.append('-k0')
+        if args.verbose:
+            invocation.append('-v')
+
+    return invocation
+
+
+def main():
+    args = _build_parser()
+
+    args.output_path = os.path.abspath(args.output_path)
+    # VS puts intermediate files in config-specific subdirs for us
+    if platform.system() == 'Windows':
+        pass
+    else:
+        args.output_path = os.path.join(args.output_path, args.build_config)
+
+    if not os.path.exists(args.output_path):
+        os.makedirs(args.output_path)
+
+    subprocess.check_call(_get_cmake_invocation(args), cwd=args.output_path)
+    subprocess.check_call(_get_build_invocation(args), cwd=root_dir)
+
+    rc = 0
+    if args.tests:
+        rc = subprocess.call(['ctest', '-C', args.build_config, '--output-on-failure'], cwd=args.output_path)
+
+    # For CI builds we want a fail return status if tests fail; test failure prevents installer creation.
+    if rc != 0:
+        sys.exit(-1)
+
+if __name__ == "__main__":
     main()
+
