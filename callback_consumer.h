fe09c5e66ef523eccc66897105da408dc9bcce9d
Jonathan B Coe <jbcoe@users.noreply.github.com>
2017-02-06 16:05:41 +0000

    Win 64 fixes

--- callback_consumer.h
+++ callback_consumer.h
@@ -1,4 +1,12 @@
+#ifdef _MBCS
+#ifdef callback_consumer_EXPORT
+#define EXPORT extern "C" __declspec(dllexport)
+#else
+#define EXPORT extern "C" __declspec(dllimport)
+#endif
+#else
 #define EXPORT __attribute__((visibility("default"))) extern "C"
+#endif
 
 EXPORT int callback_consumer_invoke(void(*callback)(void*), void* callback_input);
 
